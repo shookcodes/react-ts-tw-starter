cab78bc390100f3a315866dbdb56c84746aa92e4
Sarah Shook <srowlesdeveloper@gmail.com>
2023-05-16 22:11:01 -0700

    Fix eslint

--- eslintrc.js
+++ eslintrc.js
@@ -1,22 +1,16 @@
 module.exports = {
   root: true,
-  parser: '@babel/eslint-parser',
+  parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 2021,
     sourceType: 'module',
-    ecmaFeatures: {
-      jsx: true,
-      legacyDecorators: true, // Add this line
-    },
   },
-
   plugins: ['react', 'react-hooks', '@typescript-eslint'],
   extends: [
     'eslint:recommended',
     'plugin:react/recommended',
     'plugin:react-hooks/recommended',
     'plugin:@typescript-eslint/recommended',
-    'plugin:react/recommended',
     'prettier',
   ],
   rules: {
